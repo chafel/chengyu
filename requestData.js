256b2b476e4945d1f2823b2c2ff3d42c2ef7a6a2
chaofei <chaofeis@gmail.com>
2017-07-21 09:55:21 +0800

    get headers and handle error

--- requestData.js
+++ requestData.js
@@ -1,12 +1,38 @@
 var request = require('request');
 
 module.exports = function (key, word, cb) {
-  // const dataUrl = `http://api.avatardata.cn/ChengYu/Search?key=${key}&keyWord=${word}`;
+  const dataUrl = `http://api.avatardata.cn/ChengYu/Search?key=${key}&keyWord=${word}`;
 
-  request.get('http://api.avatardata.cn/ChengYu/Search?key=' + key + '&keyWord=' + word)
+  request.get(`http://api.avatardata.cn/ChengYu/Search?key=${key}&keyWord=${word}`)
     .on('response', function (response) {
       console.log(response.statusCode);
-      console.log(response);
+      console.log(response.headers['content-type'])
+      console.log(response.headers['host']);
+      console.log(response.headers['from']);
+
+      let error;
+      if (statusCode !== 200) {
+        error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+      }
+      if (error) {
+        console.error(error.message);
+        // consume response data to free up memory
+        res.resume();
+        return;
+      }
+
+      response.setEncoding('utf8');
+      let rawData = '';
+      response.on('data', (chunk) => { rawData += chunk; });
+      response.on('end', () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          console.log(parsedData);
+          cb(parsedData);
+        } catch (e) {
+          console.error(e.message);
+        }
+      });
     })
     .on('error', function(err) {
       console.log(err)

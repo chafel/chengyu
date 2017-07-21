b50b5c481c21af19915b356e16cb3335941f529b
chaofei <chaofeis@gmail.com>
2017-07-21 09:43:24 +0800

    simply codes

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ var request = require('./requestData');
 
 var port = Number(process.argv[2]);
 var apikey = process.argv[3];
-// target data url:
-// var dataQueryUrl = `http://api.avatardata.cn/ChengYu/Search?key=${apikey}&keyWord=`;
-
 
 var fakeMessage = {
   "text" : "##~~text, this field may accept markdown~~",
@@ -44,39 +41,3 @@ app.post('/chengyu', function(req, res) {
   });
 })
 app.listen(port);
-
-// req data format:
-// {
-//   "token" : "8d37093bb3548d9be7d876147d288337",
-//   "ts" : 1355517523,
-//   "text" : "!baike 中国",
-//   "trigger_word" : "!baike",
-//   "subdomain" : "your_domain",
-//   "channel_name" : "your_channel",
-//   "user_name" : "your_name"
-// }
-
-//外部服务可以通过验证请求数据中的 token 是否为 8d37093bb3548d9be7d876147d288337 来判断请求有效性。
-
-/*外部服务可以返回一个 Response，如果格式满足下面的样例，Outgoing 会发送一条对应的消息到讨论组内
-
-Response:
-
-{
-  "text" : "text, this field may accept markdown",
-  "attachments" : [
-  {
-    "title" : "title_1",
-    "text" : "attachment_text",
-    "color" : "#666666",
-    "images" : [
-      {
-        "url" : "http://example.com/index.jpg"
-      },
-      {
-        "url" : "http://example.com/index.jpg"
-      }
-    ]
-  }
-]
-}*/
--- requestData.js
+++ requestData.js
@@ -1,69 +1,14 @@
-// var http = require('http');
 var request = require('request');
-// const url = require('url');
-//var dataQueryUrl = `http://api.avatardata.cn/ChengYu/Search?key=${apikey}&keyWord=`;
 
-function request(key, word, cb) {
+module.exports = (key, word, cb) {
   const dataUrl = `http://api.avatardata.cn/ChengYu/Search?key=${key}&keyWord=${word}`;
 
-// Set the headers
-  var headers = {
-    // 'User-Agent':       'Super Agent/0.0.1',
-    'Content-Type':     'application/json'
-  }
-
-// Configure the request
-  // var options = {
-  //   url: `http://api.avatardata.cn/ChengYu/Search?key=${key}&keyWord=${word}`,
-  //   method: 'GET',
-  //   headers: headers,
-  // }
-
-// Start the request
-  request.get(dataUrl)
+  request.get(`http://api.avatardata.cn/ChengYu/Search?key=${key}&keyWord=${word}`)
     .on('response', function (response) {
-      console.log(response.statusCode) // 200
+      console.log(response.statusCode);
       console.log(response);
     })
     .on('error', function(err) {
       console.log(err)
-    })
-
-  // console.log(dataUrl);
-  // http.get(dataUrl, (res) => {
-  //   console.log(res);
-  //   const { statusCode } = res;
-  //   const contentType = res.headers['content-type'];
-  //
-  //   let error;
-  //   if (statusCode !== 200) {
-  //     error = new Error('Request Failed.\n' +
-  //       `Status Code: ${statusCode}`);
-  //   } else if (!/^application\/json/.test(contentType)) {
-  //     error = new Error('Invalid content-type.\n' +
-  //       `Expected application/json but received ${contentType}`);
-  //   }
-  //   if (error) {
-  //     console.error(error.message);
-  //     // consume response data to free up memory
-  //     res.resume();
-  //     return;
-  //   }
-  //   res.setEncoding('utf8');
-  //   let rawData = '';
-  //   res.on('data', (chunk) => { rawData += chunk; });
-  //   res.on('end', () => {
-  //     try {
-  //       const parsedData = JSON.parse(rawData);
-  //       console.log(parsedData);
-  //       cb(parsedData);
-  //     } catch (e) {
-  //       console.error(e.message);
-  //     }
-  //   });
-  // }).on('error', (e) => {
-  //     console.error(`Got error: ${e.message}`);
-  // });
+    });
 }
-
-module.exports = request;

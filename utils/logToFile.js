8ae2e0a2ee1c47d3f2f01373dfca1c7d3a2f09c5
chaofei <chaofeis@gmail.com>
2017-07-22 15:10:39 +0800

    update log function

--- utils/logToFile.js
+++ utils/logToFile.js
@@ -3,10 +3,18 @@ var util = require('util');
 var log_stdout = process.stdout;
 
 module.exports = function(dirname) {
-  var log_file = fs.createWriteStream(dirname, {flags : 'w'});
+  // var log_file = fs.createWriteStream(dirname, {flags : 'w'});
 
-  global.console.log = function(d) { //
-    log_file.write(new Date() + ' :\n' + util.format(d) + '\n');
-    log_stdout.write(util.format(d) + '\n');
+  global.console.log = function() { //
+    // log_file.write(new Date() + ' :\n' + util.format.apply(null, arguments) + '\n');
+    var data = new Date() + ' :\n' + util.format.apply(null, arguments) + '\n';
+    fs.appendFile(dirname, data, function (err) {
+      if (err) throw err;
+      console.log('Saved to log!');
+    });
+
+    log_stdout.write(util.format.apply(null, arguments) + '\n');
   };
+
+  console.error = console.log;
 }
